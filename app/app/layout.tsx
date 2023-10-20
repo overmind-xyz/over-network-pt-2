604af1bd13a14b1d9fa9e852bee1a7413ceac0e4
Ifhezu <90856703+Ifhezu@users.noreply.github.com>
2023-10-20 12:57:25 +0600

    Update layout.tsx
    Ok, now its updated

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,5 +1,5 @@
 import './globals.css';
-import { Inter } from 'next/font/google';
+import { Inter } from 'next/font/google'; 
 import Accounts from '@/components/Accounts';
 import localFont from 'next/font/local';
 import { Avatar } from '@/components/avatar';
@@ -57,6 +57,7 @@ export default async function RootLayout({
     /*
       TODO #1: Indicate that this function is a server function by adding 'use server';
     */
+    use server;
 
     /*
       TODO #2: Create the new User object with a username, name, and privateKey
@@ -65,13 +66,20 @@ export default async function RootLayout({
         - 
         - Use the newPrivateKey() function to generate a new private key for the user
     */
-
+  const privateKey = newPrivateKey();
+  const user = {
+    username: form.get('username') as string,
+    name: form.get('name') as string,
+    privateKey,
+  };
     /* 
       TODO #3: Store the user in the local account cache
 
       HINT: Use the storeUser() function to store the user
     */
+storeUser(user);
 
+  try {
     /* 
       TODO #4: Set up a try catch block to create the user's profile and log them in if successful.
 
@@ -83,6 +91,12 @@ export default async function RootLayout({
           from the local account cache. Then, throw the error to be caught by the catch block in
           the loginWindow.tsx file.
     */
+     await createProfile(user);
+    await login(user);
+  } catch (error) {
+    dropUser(user);
+    throw error;
+  }
   }
 
   if (!me) {
